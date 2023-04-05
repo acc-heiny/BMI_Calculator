04a160db7bea98e5adbaf9e0fee4abea43d7eeca
Hellen Heiny <hellen.heiny.f.santi@accenture.com>
2023-04-05 10:48:57 -0300

    Code review

--- DesafioIMC-2/Controller/ResultViewController.swift
+++ DesafioIMC-2/Controller/ResultViewController.swift
@@ -2,12 +2,11 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    var bmiResult: Double
+
     var bmiResultView: BMIResultView
     var bmiResultModel: ShowResultModel
     
     init(bmiResultCalculated: Double){
-        self.bmiResult = bmiResultCalculated
         bmiResultModel = ShowResultModel(bmiResult: bmiResultCalculated)
         bmiResultView = BMIResultView(setup: bmiResultModel.setup, bmiResult: bmiResultCalculated)
         super.init(nibName: nil, bundle: nil)
--- DesafioIMC-2/SceneDelegate.swift
+++ DesafioIMC-2/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         let viewController: ViewController = ViewController()
-        let navigationController:UINavigationController = UINavigationController(rootViewController: viewController)
+        let navigationController: UINavigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         window.windowScene = windowScene
         window.makeKeyAndVisible()
